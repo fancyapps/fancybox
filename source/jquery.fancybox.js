3ae3ef55fa7e4df608527f69f79f7d9017150c48
Janis Skarnelis <janis.skarnelis@gmail.com>
2011-12-01 14:18:59 +0200

    Fixed #32 - ignore key combinations

--- source/jquery.fancybox.js
+++ source/jquery.fancybox.js
@@ -394,22 +394,24 @@
 
 			if (keys) {
 				D.bind('keydown.fb', function (e) {
-					// Ignore key events within form elements
-					if ($.inArray(e.target.tagName.toLowerCase(), ['input', 'textarea', 'select', 'button']) > -1) {
-						return;
-					}
+					var code;
 
-					if ($.inArray(e.keyCode, keys.close) > -1) {
-						F.close();
-						e.preventDefault();
+					// Ignore key combinations and key events within form elements
+					if (!e.ctrlKey && !e.altKey && !e.shiftKey && !e.metaKey && $.inArray(e.target.tagName.toLowerCase(), ['input', 'textarea', 'select', 'button']) < 0) {
+						code = e.keyCode;
 
-					} else if ($.inArray(e.keyCode, keys.next) > -1) {
-						F.next();
-						e.preventDefault();
+						if ($.inArray(code, keys.close) > -1) {
+							F.close();
+							e.preventDefault();
 
-					} else if ($.inArray(e.keyCode, keys.prev) > -1) {
-						F.prev();
-						e.preventDefault();
+						} else if ($.inArray(code, keys.next) > -1) {
+							F.next();
+							e.preventDefault();
+
+						} else if ($.inArray(code, keys.prev) > -1) {
+							F.prev();
+							e.preventDefault();
+						}
 					}
 				});
 			}
