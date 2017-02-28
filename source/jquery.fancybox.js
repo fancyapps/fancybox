e18ca06ae0b61595691823034507d4f66710776f
Janis Skarnelis <janis.skarnelis@gmail.com>
2017-02-28 17:39:55 +0200

    Update bower and version numbers

--- source/jquery.fancybox.js
+++ source/jquery.fancybox.js
@@ -1,12 +1,12 @@
 /*!
  * fancyBox - jQuery Plugin
- * version: 2.1.5 (Fri, 14 Jun 2013)
+ * version: 2.1.7 (Tue, 28 Feb 2017)
  * requires jQuery v1.6 or later
  *
  * Examples at http://fancyapps.com/fancybox/
  * License: www.fancyapps.com/fancybox/#license
  *
- * Copyright 2012 Janis Skarnelis - janis@fancyapps.com
+ * Copyright 2017 fancyapps.com
  *
  */
 
@@ -50,7 +50,7 @@
 
 	$.extend(F, {
 		// The current version of fancyBox
-		version: '2.1.5',
+		version: '2.1.7',
 
 		defaults: {
 			padding : 15,
